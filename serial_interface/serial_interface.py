38818216265b65468d085c20c762a8b8b8f56315
Peter Polidoro <peterpolidoro@gmail.com>
2021-03-01 14:29:15 -0500

    Windows does not recognize serial flush

--- serial_interface/serial_interface.py
+++ serial_interface/serial_interface.py
@@ -182,7 +182,6 @@ class SerialInterface(serial.Serial):
         if not lock_acquired:
             raise WriteFrequencyError("Time between writes needs to be larger.")
         try:
-            self.flush()
             bytes_written = 0
             if check_write_freq:
                 bytes_written = self.write_check_freq(data,delay_write=delay_write,lock_=False)
