b9a8ab26584c40a00557eb18d1ca882688653897
Peter Polidoro <peter@polidoro.io>
2021-09-17 10:28:20 -0400

    Update email address

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     url='https://github.com/janelia-pypi/serial_interface_python',
 
     author='Peter Polidoro',
-    author_email='peterpolidoro@gmail.com',
+    author_email='peter@polidoro.io',
 
     license='BSD',
 
